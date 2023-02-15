4a50628928c083e87be5e613da17312262194140
Praveen Singh <praveensingh.name@gmail.com>
2023-02-16 01:10:22 +0530

    folder chnages

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/card-animation/CardAnimation';
+import App from './playground/card-animation/CardAnimation';
 
-// const root = document.getElementById('root');
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-//     root
-// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-// root.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
